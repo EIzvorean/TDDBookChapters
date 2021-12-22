c92086de6611099c715558c7421d934fd72bb8db
EIzvorean <edward.izvorean@gmail.com>
2021-12-21 23:27:02 -0500

    Chpt7 - First steps of FT for layout nd styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -45,7 +45,6 @@ class NewVisitorTest(LiveServerTestCase):
         # is tying fly-fishing lures)
         inputbox.send_keys('Buy peacock feathers')
         
-
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
@@ -69,7 +68,7 @@ class NewVisitorTest(LiveServerTestCase):
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
 
 
-        self.fail('Finish the test!')
+        # self.fail('Finish the test!')
         
         # The page updates again, and now shows both items on her list
 
@@ -123,6 +122,22 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         #Satisfied, they both go back to sleep
+
+    def test_layout_and_styling(self):
+        #Edit goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        #She notices the inpux bos is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10)
+
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10)
+
 
 
 # if __name__ == '__main__':  
